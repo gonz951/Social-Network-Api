dd02b7145ec203e4a197408bd8ac7b1cc4b0c586
Fel <felgonzalez951@gmail.com>
2024-07-09 13:36:42 -0600

    added two basic routes to start off with, will test with seed data

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,28 @@
-// add code later
+const { User, Thought } = require('../models');
+
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+              .select('-__v');
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with that id.' })
+            }
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    // add more after verifying this works
+}
