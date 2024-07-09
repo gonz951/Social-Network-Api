b064fa66d22c5a10b86ae09d52ce72fdd31e5b6e
Fel <felgonzalez951@gmail.com>
2024-07-09 14:08:53 -0600

    working on seeding, still a WIP

--- utils/data.js
+++ utils/data.js
@@ -1,2 +1,29 @@
 // should hold all the actual seed data
 // fill in later
+const usernames = [
+    'u354dldr',
+    'lr509f31',
+    'g10vecroisan7',
+    'n53m3t4t',
+    'ezeewts',
+    'ariesladybug',
+    'lihcorts',
+    'kelpmustard',
+    '285y',
+    'oldeuboisn4ke',
+    'chocyMelk',
+];
+
+const getRandomArrItem = (arr) = arr[Math.floor(Math.random() * arr.length)];
+
+const getRandomUser = () => 
+    `${getRandomArrItem(usernames)}`;
+
+const getRandomThoughts = (int) => {
+    let results = [];
+    for (let i = 0; i < int; i++) {
+        results.push({
+           // add later 
+        })
+    }
+}
--- utils/seed.js
+++ utils/seed.js
@@ -1,4 +1,23 @@
 const connection = require('../config/connection');
 const { User, Thought } = require('../models');
+const { getRandomUser, getRandomThoughts } = require('./data');
 
 // add the rest later
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    console.log('connected');
+
+    let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+    if (thoughtCheck.length) {
+        await connection.dropCollection('thoughts');
+    }
+
+    let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+    if (userCheck.length) {
+        await connection.dropCollection('users');
+    }
+
+    const users = [];
+    const thoughts = getRandomThoughts(10);
+})
