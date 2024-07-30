92ac6b15b8c80d359c6115be6893566c9c28c73e
Fel <felgonzalez951@gmail.com>
2024-07-30 14:45:17 -0600

    added route methods to thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
 // ! Need to make methods used here in controllers
-
+const {
+    getThoughts, 
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction 
+} = require('../../controllers/thoughtController');
 // ! Need to build out routes later
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
