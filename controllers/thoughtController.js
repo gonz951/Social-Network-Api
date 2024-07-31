f975c358b2c6b5666d1a85679faa56fca4d56885
Fel <felgonzalez951@gmail.com>
2024-07-30 23:09:31 -0600

    added createReaction and deleteReaction + need to tweak deleteReactioin

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -79,5 +79,39 @@ module.exports = {
         } catch (err) {
             res.status(500).json(err)
         }
-    }
-}
+    },
+    async createReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $addToSet: { reactions: req.body } },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that id.' });
+            }
+            
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $pull: { reactions: { reactionId: req.params.reactionId } } },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that id.' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+};
