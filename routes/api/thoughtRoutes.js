1e9e7d48e34527cb0c304576b78425e13778ade4
Fel <felgonzalez951@gmail.com>
2024-07-30 22:20:08 -0600

    uncommenting thoughts as they're being made + need to tweak updateUser

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,20 +1,19 @@
 const router = require('express').Router();
-// ! Need to make methods used here in controllers
 const {
     getThoughts, 
     getSingleThought,
     createThought,
-    // updateThought,
-    // deleteThought,
+    updateThought,
+    deleteThought,
     // createReaction,
     // deleteReaction 
 } = require('../../controllers/thoughtController');
-// ! Need to build out routes later
+
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route('/:thoughtId').get(getSingleThought)//.put(updateThought).delete(deleteThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 // // /api/thoughts/:thoughtId/reactions
 // router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction)
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,18 +3,18 @@ const {
     getUsers,
     getSingleUser,
     createUser,
-    // updateUser,
-    // deleteUser,
+    // ! update User is finicky
+    updateUser,
+    deleteUser,
     // addFriend,
     // deleteFriend,
 } = require('../../controllers/userController');
 
-// ! Need to build out routes later
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser)//.put(updateUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 // // /api/users/:userId/friends/:friendId
 // router.route('./:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
